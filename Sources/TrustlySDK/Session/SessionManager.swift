4ebe23b7dc6fb2ef473f85681f4ff455cdd2242d
Marcos Rivereto <marcos.rivereto@trustly.com>
2023-06-05 10:41:32 -0300

    Fix file location

---
+++ Sources/TrustlySDK/Session/SessionManager.swift
@@ -0,0 +1,37 @@
+//
+//  SessionManager.swift
+//  TrustlySDK
+//
+//  Created by Marcos Rivereto on 01/06/23.
+//
+
+import Foundation
+
+
+struct SessionCid {
+    let sessionId: String
+    let experidTime: Date
+}
+
+//TODO: Create cid
+func generateCid() -> String {
+    return ""
+}
+
+//TODO: Return sessionCid
+func getOrCreateSessionCid(_ cid: String) -> String {
+    
+    return ""
+}
+
+//public class SessionManager: NSObject {
+//
+//    //TODO: Return cid
+////    private var cid: String {
+////        get { return self.generateCid() }
+////    }
+//
+//
+//    //TODO: Check sessionCid
+//    //TODO: store sessionCid
+//}
