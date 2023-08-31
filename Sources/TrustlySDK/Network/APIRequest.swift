8a35371249386f8942093a75de9042f78f41217b
Marcos Rivereto <marcos.rivereto@trustly.com>
2023-08-31 10:59:26 -0300

    Change passkey to use fly.io backend version

--- Sources/TrustlySDK/Network/APIRequest.swift
+++ Sources/TrustlySDK/Network/APIRequest.swift
@@ -73,7 +73,6 @@ struct RP: Codable, Hashable {
 
 public struct PassKeyResult: Codable, Hashable {
     let status: String?
-    let message: String?
     let challenge: String?
     let message: String?
     
@@ -125,50 +124,4 @@ struct APIRequest {
         return passKeyResult
     }
     
-//    func doRequest(address: String, httpMethod: String, token: String?) {
-//        var request = URLRequest(url: URL(string: self.getUrl(address: address))!)
-//
-//        print(request)
-//
-//        //You can pass any required content types her
-//        request.httpMethod = httpMethod
-//
-//        // Some bad practice hard coding tokens in the Controller code instead of keychain.
-//        if let access_token = token {
-//            //You endpoint is setup as OAUTH 2.0 and we are sending Bearer token in Authorization header
-//            request.setValue("Bearer \(access_token)", forHTTPHeaderField: "Authorization")
-//
-//        }
-//ApiCli
-//        let session = URLSession.shared
-//
-//        session.dataTask(with: request) { data, response, err in
-//
-//            do{
-//
-//                let JSON = try JSONSerialization.jsonObject(with: data!, options: []) as! NSDictionary
-//
-//                let dataSet = (JSON.value(forKeyPath: "parentNode-childNode") as! NSArray).object(at: 0) as! NSDictionary
-//
-//                DispatchQueue.main.async {
-//
-//                    let Val = dataSet.value(forKey: "subChildNode")
-//
-//                    // This is specific to the graph UI that is used, and requires CGFloat type.
-//
-//                    //currentSteps is my graph UI.
-//
-////                    self.currentSteps.setValue(Val as! CGFloat, animateWithDuration: 1)
-//
-//                }
-//
-//            } catch {
-//
-//                print("json error: \(error)")
-//
-//            }
-//
-//        }.resume()
-//
-//    }
 }
