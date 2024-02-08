8412c07a01c60454e41afcbc337c7394fb8f9fe4
Marcos Rivereto <marcos.rivereto@trustly.com>
2024-02-08 11:43:50 -0300

    Create JSONUtils to convert dictionary to json objects

---
+++ Example/Tests/JsonUtilsTests.swift
@@ -0,0 +1,46 @@
+//
+//  JsonUtilsTests.swift
+//  TrustlySDK_Tests
+//
+//  Created by Marcos Rivereto on 08/02/24.
+//  Copyright © 2024 CocoaPods. All rights reserved.
+//
+
+import XCTest
+@testable import TrustlySDK
+
+final class JsonUtilsTests: XCTestCase {
+    
+    var establishData:Dictionary<String, String> = [:]
+    
+    override func setUp() {
+        super.setUp()
+        
+        self.establishData = ["accessId": "A48B73F694C4C8EE6306",
+                                "merchantId" : "110005514"]
+        
+    }
+    
+    func testBuildJsonString() throws {
+        
+        let expectedString = "{\"merchantId\":\"110005514\",\"accessId\":\"A48B73F694C4C8EE6306\"}".hash
+
+        
+        let jsonString = JSONUtils.getJsonStringFrom(dicticionary: self.establishData)!
+        
+        XCTAssertEqual(expectedString, jsonString.hash)
+                
+    }
+    
+    func testBuildJsonBase64() throws {
+        
+        let jsonStringBase64 = JSONUtils.getJsonBase64From(dicticionary: self.establishData)!
+        let newDictionary = JSONUtils.getDictionaryFrom(jsonStringBase64: jsonStringBase64)!
+        
+        
+        XCTAssertEqual(newDictionary, self.establishData)
+                
+    }
+
+
+}
