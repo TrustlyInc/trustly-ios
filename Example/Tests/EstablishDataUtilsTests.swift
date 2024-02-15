06539328d52b41cab12a5f7d0dd15bee2af7dcb0
Marcos Rivereto <marcos.rivereto@trustly.com>
2024-02-15 14:47:01 -0300

    Create EstablishDataUtils to normalize establish format

---
+++ Example/Tests/EstablishDataUtilsTests.swift
@@ -0,0 +1,40 @@
+//
+//  EstablishDataUtilsTests.swift
+//  TrustlySDK_Tests
+//
+//  Created by Marcos Rivereto on 15/02/24.
+//  Copyright © 2024 CocoaPods. All rights reserved.
+//
+
+import XCTest
+@testable import TrustlySDK
+
+final class EstablishDataUtilsTests: XCTestCase {
+    
+    var establishData:Dictionary<String, String> = [:]
+    
+    override func setUp() {
+        super.setUp()
+        
+        self.establishData = ["accessId": "A48B73F694C4C8EE6306",
+                                "customer.address.country" : "US",
+                                "customer.address.street" : "ABC Avenue",
+                                "metadata.urlScheme": "demoapp://"]
+        
+    }
+    
+    func testNormalizeDotNotation() throws {
+        
+        let expectedEstablishData: [String : AnyHashable] = [
+            "accessId": "A48B73F694C4C8EE6306",
+            "customer" : ["address": ["country": "US", "street": "ABC Avenue"]],
+            "metadata" : ["urlScheme": "demoapp://"]
+        ]
+        
+        let jsonString = EstablishDataUtils.normalizeEstablishWithDotNotation(establish: self.establishData)
+        
+        XCTAssertEqual(expectedEstablishData, jsonString)
+                
+    }
+
+}
