b1aa1187d5edfab3608f312ddbeceaf523305faa
Marcos Rivereto <marcos.rivereto@trustly.com>
2024-02-20 10:30:04 -0300

    Fix multiple PR comments

--- Example/TrustlySDK/ViewController.swift
+++ Example/TrustlySDK/ViewController.swift
@@ -85,7 +85,7 @@ class ViewController: BaseViewController {
         
     }
     
-    @IBAction func payWituAESWebAuth(_ sender: Any) {
+    @IBAction func payWithAESWebAuth(_ sender: Any) {
 
         if let amountText = amountTextView.text,
            let amount = Double(amountText) {
