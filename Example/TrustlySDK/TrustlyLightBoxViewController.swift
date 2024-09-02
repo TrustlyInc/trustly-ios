fb023ee58694d9fb64ade5bd69d1c011749da3b4
Marcos Rivereto <marcos.rivereto@trustly.com>
2024-09-02 13:35:28 -0300

    Remove comments code.

--- Example/TrustlySDK/TrustlyLightBoxViewController.swift
+++ Example/TrustlySDK/TrustlyLightBoxViewController.swift
@@ -18,25 +18,7 @@ class TrustlyLightBoxViewController: UIViewController {
     var delegate: TrustlyLightboxViewProtocol?
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
-//        let trustlyLightboxPanel = TrustlyView()
-//        
-//        if let establishData = self.establishData {
-//            self.view = trustlyLightboxPanel.establish(establishData: establishData,
-//                                                       onReturn: {(payWithMyBank, returnParameters)->Void in
-//                let response = returnParameters as! [String:String]
-//                self.delegate?.onReturnWithTransactionId(transactionId: response["transactionId"]!, controller: self)
-//                
-//            }, onCancel: {(payWithMyBank, returnParameters)->Void in
-//                let response = returnParameters as! [String:String]
-//                self.delegate?.onCancelWithTransactionId(transactionId: response["transactionId"] ?? "No Transaction ID", controller: self)
-//            })
-//            
-//        } else {
-//            self.delegate?.onCancelWithTransactionId(transactionId: "Empty establishData", controller: self)
-//        }
-        
+        super.viewDidLoad()     
     }
     
     override func viewDidAppear(_ animated: Bool) {
