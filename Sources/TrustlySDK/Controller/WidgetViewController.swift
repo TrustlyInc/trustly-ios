6a6e41debbd093c14f970a915cd9766a06521ad4
Marcos Rivereto <marcos.rivereto@trustly.com>
2025-02-11 09:34:37 -0300

    Create WidgetViewController.swift

--- Sources/TrustlySDK/Controller/WidgetViewController.swift
+++ Sources/TrustlySDK/Controller/WidgetViewController.swift
@@ -1,106 +1,56 @@
-/*  ___________________________________________________________________________________________________________
- *
- *    TRUSTLY CONFIDENTIAL AND PROPRIETARY INFORMATION
- *  ___________________________________________________________________________________________________________
- *
- *      Copyright (c) 2012 - 2020 Trustly
- *      All Rights Reserved.
- *
- *   NOTICE:  All information contained herein is, and remains, the confidential and proprietary property of
- *   Trustly and its suppliers, if any. The intellectual and technical concepts contained herein are the
- *   confidential and proprietary property of Trustly and its suppliers and  may be covered by U.S. and
- *   Foreign Patents, patents in process, and are protected by trade secret or copyright law. Dissemination of
- *   this information or reproduction of this material is strictly forbidden unless prior written permission is
- *   obtained from Trustly.
- *   ___________________________________________________________________________________________________________
-*/
+//
+//  WidgetViewController.swift
+//  Pods-TrustlySDK_Example
+//
+//  Created by Marcos Rivereto on 31/01/25.
+//
 
 import Foundation
 import UIKit
-@preconcurrency import WebKit
+
+public protocol WidgetViewControllerProtocol: AnyObject {
+    /*!
+        @brief Sets a callback to handle external URLs
+        @param onExternalUrl Called when the TrustlySDK panel must open an external URL. If not handled an internal in app WebView will show the external URL.The external URL is sent on the returnParameters entry key “url”.
+    */
+    func onExternalUrl(onExternalUrl: TrustlyCallback?) -> Void;
+    
+    /*!
+        @brief Sets a callback to handle event triggered by javascript
+        @param eventName Name of the event.
+        @param eventDetails Dictionary with information about the event.
+    */
+    func onChangeListener(_ eventName: String, _ eventDetails: [AnyHashable : Any]) -> Void;
+    
+}
 
 
 public class WidgetViewController: UIViewController {
     
-    private var establishData: [AnyHashable : Any]?
-    private var mainWebView:WKWebView!
-    private var webViewManager: WebViewManager?
-    
-    public weak var delegate: TrustlySDKProtocol?
-    
-    public init(establishData: [AnyHashable : Any]){
-        super.init(nibName: nil, bundle: nil)
+    public weak var delegate: WidgetViewControllerProtocol?
+    var trustlyView:TrustlyView = TrustlyView()
         
-        self.establishData = establishData
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
-    
     public override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.initWebView()
+        self.view.backgroundColor = .cyan
         
-        self.webViewManager?.onChangeListener { (eventName, attributes) in
+        self.trustlyView.onChangeListener { (eventName, attributes) in
             self.delegate?.onChangeListener(eventName, attributes)
+            
+            print("onChangeListener: \(eventName) \(attributes)")
         }
-        
-        self.selectBankWidget()
-    }
-    
-    func initWebView() {
-
-        webViewManager = WebViewManager()
-        
-        let configuration = WKWebViewConfiguration()
-        let userController = WKUserContentController()
-        
-        guard let webViewManager = self.webViewManager else { return }
-        
-        userController.add(webViewManager, name: Constants.messageWebviewHandler)
-        configuration.userContentController = userController
-        
-        let wkPreferences = WKPreferences()
-        wkPreferences.javaScriptCanOpenWindowsAutomatically = true
-        
-        configuration.preferences = wkPreferences
-
-        let frame = CGRect(x:0, y:0, width: self.view.frame.size.width, height: self.view.frame.size.height)
-        mainWebView = WKWebView(frame:frame, configuration:configuration)
-        mainWebView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        mainWebView.navigationDelegate = webViewManager
-        mainWebView.uiDelegate = webViewManager
-        mainWebView.scrollView.bounces = false
-        mainWebView.backgroundColor = UIColor.clear
-        mainWebView.isOpaque = false
-        
-        webViewManager.mainWebView = mainWebView
-        
-        if #available(iOS 16.4, *) {
-            mainWebView.isInspectable = true
-        }
-
-        self.view.addSubview(mainWebView)
     }
 }
 
 extension WidgetViewController {
 
-    private func selectBankWidget() {
-        
-        let service = TrustlyService()
-        
-        self.webViewManager?.establishData = establishData
-        
-        if let establishData = establishData,
-           let urlRequest = service.selectBankWidget(establishData: establishData) {
-            self.mainWebView.load(urlRequest)
-        }
-        
-        self.webViewManager?.bankSelectedHandler = { establishData in
-            self.delegate?.onBankSelected(data: establishData)
+    public func selectBankWidget(establishData: [AnyHashable : Any], onBankSelected: @escaping TrustlyCallback) {
+        self.view = self.trustlyView.selectBankWidget(establishData: establishData) { data in
+            print("returnParameters:\(data)")
+            
+            onBankSelected(data)
+
         }
     }
 }
