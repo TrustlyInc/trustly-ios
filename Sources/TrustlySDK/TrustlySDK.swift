2280c8875831700996d9d343b50bbeefd9f44652
Marcos Rivereto <marcos.rivereto@trustly.com>
2025-02-11 09:40:31 -0300

    Change TrustlySDK signature

--- Sources/TrustlySDK/TrustlySDK.swift
+++ Sources/TrustlySDK/TrustlySDK.swift
@@ -7,7 +7,7 @@
 import Foundation
 import UIKit
 
-public typealias TrustlyCallback = (_ trustlyView: UIView, _ returnParameters: [AnyHashable : Any]) -> Void;
+public typealias TrustlyCallback = (_ returnParameters: [AnyHashable : Any]) -> Void;
 public typealias ExternalUrlCallback = (_ request: NSURLRequest?) -> Void?;
 public typealias TrustlyListenerCallback = (_ eventName: String, _ eventDetails: [AnyHashable : Any]) -> Void;
 
@@ -29,20 +29,11 @@ protocol TrustlyProtocol {
         @param estabilishData The dictionary containing the establish data. Similar to the establishData object on the JavaScript API.
         @param onBankSelected Called when the user selects a bank on the widget. The callback returnParameters dictionaryis the mutable clone of the establishData with an additional entry for the key “paymentProviderId” with the id of the selected bank.
     */
-    func selectBankWidget(establishData: [AnyHashable : Any], onBankSelected: TrustlyCallback?) -> UIView?;
+    func selectBankWidget(establishData: [AnyHashable : Any], onBankSelected: @escaping TrustlyCallback);
 
 
     func verify(verifyData: [AnyHashable : Any], onReturn:TrustlyCallback?, onCancel: TrustlyCallback? ) -> UIView?;
 
-    /*!
-        @brief Shows a hybrid page that will establish a new transaction
-        @param url Initial page URL.
-        @param returnUrl returnUrl parameter set on the establish data
-        @param onReturn Called when the user flow finishes and control is returned to the application.The returnParameters has as entries the same parameters of the returnURL on the web (check the definition on the Integration Guide).
-        @param cancelUrl cancelUrl parameter set on the establish data
-        @param onCancel Called when the user cancels the flow and the control is returned to the application.The returnParameters has as entries the same parameters of the returnURL on the web (check the definition on the Integration Guide)
-    */
-    func hybrid(url: String, returnUrl: String, onReturn: TrustlyCallback?, cancelUrl: String, onCancel: TrustlyCallback?) -> UIView?;
 
     /*!
         @brief Sets a callback to handle external URLs
