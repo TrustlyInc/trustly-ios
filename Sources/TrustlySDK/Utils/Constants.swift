352530e74cf8a4baaf24de5ec6492817f91ab7de
Marcos Rivereto <marcos.rivereto@trustly.com>
2025-03-06 14:36:45 -0300

    Fix sonar warnings

--- Sources/TrustlySDK/Utils/Constants.swift
+++ Sources/TrustlySDK/Utils/Constants.swift
@@ -12,25 +12,27 @@ struct Constants {
     static let buildSDK = "3.3.0"
     static let baseDomain = "paywithmybank.com"
     
-    static let RETURN_URL = "msg://return"
-    static let CANCEL_URL = "msg://cancel"
-    static let ESTABLISH_VERSION = "2"
-    static let DEVICE_TYPE = "mobile"
-    static let DEVICE_PLATFORM = "ios:native"
-    static let PAYMENTTYPE_VERIFICATION = "Verification"
+    static let returnURL = "msg://return"
+    static let cancelURL = "msg://cancel"
+    static let establishVersion = "2"
+    static let deviceType = "mobile"
+    static let devicePlatform = "ios:native"
+    static let paymentTypeVerification = "Verification"
     
-    static let LIGHTBOX_CONTEXT_INAPP = "in-app-browser"
-    static let LIGHTBOX_CONTEXT_WEBVIEW = "webview"
+    static let lightboxContentInApp = "in-app-browser"
+    static let lightboxContentWebview = "webview"
     
-    static let SETTINGS_CACHE_TIME_LIMIT = 15 // minutes
+    static let settingsCacheTimeLimit = 15 // minutes
     
-    static let PORT_API = "8000"
-    static let PORT_FRONTEND = "10000"
+    static let portApi = "8000"
+    static let portFrontend = "10000"
     
-    static let IN_APP_INTEGRATION_CONTEXT = "InAppBrowser"
-    static let MESSAGE_WEBVIEW_HANDLER = "PayWithMyBankNativeSDK"
+    static let inAppIntegrationContext = "InAppBrowser"
+    static let messageWebviewHandler = "PayWithMyBankNativeSDK"
     
-    static let WIDGET_PAGE = "widget"
-    static let LOADING_TYPE = "loading"
+    static let widgetPage = "widget"
+    static let loadingType = "loading"
+    
+    static let undefinedURI = "/undefined"
     
 }
