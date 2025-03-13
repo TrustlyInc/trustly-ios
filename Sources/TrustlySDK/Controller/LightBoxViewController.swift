c5cbf1db603b603ef77197ec752646f2a74aca53
Marcos Rivereto <marcos.rivereto@trustly.com>
2025-03-13 10:19:24 -0300

    Open lightbox in WebView

--- Sources/TrustlySDK/Controller/LightBoxViewController.swift
+++ Sources/TrustlySDK/Controller/LightBoxViewController.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import WebKit
 
 public protocol LightBoxViewControllerProtocol: AnyObject {
     /*!
@@ -27,34 +28,125 @@ public protocol LightBoxViewControllerProtocol: AnyObject {
 public class LightBoxViewController: UIViewController {
     
     public weak var delegate: LightBoxViewControllerProtocol?
-    var trustlyView:TrustlyView = TrustlyView()
+    
+    private var mainWebView:WKWebView!
+    private var webViewManager: WebViewManager?
+    private let loading = UIActivityIndicatorView()
+
         
     public override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .cyan
+        self.initWebView()
         
-        self.trustlyView.onChangeListener { (eventName, attributes) in
+        self.webViewManager?.onChangeListener { (eventName, attributes) in
             self.delegate?.onChangeListener(eventName, attributes)
             
             print("onChangeListener: \(eventName) \(attributes)")
         }
     
     }
+    
+    func initWebView() {
+        
+        let configuration = WKWebViewConfiguration()
+        
+        let frame = CGRect(x:0, y:0, width: self.view.frame.size.width, height: self.view.frame.size.height)
+        mainWebView = WKWebView(frame:frame, configuration:configuration)
+        
+        webViewManager = WebViewManager(webView: mainWebView)
+        webViewManager?.notifyEvent(Constants.widgetPage, Constants.loadingType)
+        
+        let userController = WKUserContentController()
+        
+        if let webViewManager = webViewManager {
+            userController.add(webViewManager, name: Constants.messageWebviewHandler)
+        }
+        
+        configuration.userContentController = userController
+        
+        let wkPreferences = WKPreferences()
+        wkPreferences.javaScriptCanOpenWindowsAutomatically = true
+        
+        configuration.preferences = wkPreferences
+
+        mainWebView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        mainWebView.navigationDelegate = webViewManager
+        mainWebView.uiDelegate = webViewManager
+        mainWebView.scrollView.bounces = false
+        mainWebView.backgroundColor = UIColor.clear
+        mainWebView.isOpaque = false
+        
+        if #available(iOS 16.4, *) {
+            mainWebView.isInspectable = true
+        }
+
+        self.view = self.mainWebView
+    }
 }
 
 extension LightBoxViewController {
     
     public func establish(establishData: [AnyHashable : Any], onReturn: TrustlyViewCallback?, onCancel: TrustlyViewCallback?) {
-        self.view = trustlyView.establish(establishData: establishData,
-                                                   onReturn: { returnParameters ->Void in
-            print(returnParameters)
-            onReturn?(returnParameters)
-
-        }, onCancel: { returnParameters ->Void in
-            print(returnParameters)
-            onCancel?(returnParameters)
+        
+        self.startLoading()
+        
+        let service = TrustlyService()
+        service.delegate = self
+        
+        self.webViewManager?.establishData = establishData
+        self.webViewManager?.cancelHandler = onCancel
+        self.webViewManager?.returnHandler = onReturn
+        
+        service.chooseIntegrationStrategy(establishData: establishData, completionHandler: { integrationStrategy -> Void in
+          
+            if integrationStrategy == Constants.lightboxContentWebview {
+                service.establishWebView(establishData: establishData)
+                
+            } else {
+                //TODO: oAuth
+                print("oAuth")
+            }
         })
+    }
+    
+    // MARK: Support functions
+    private func startLoading() {
+        self.loading.frame = CGRectMake(0.0, 0.0, 40.0, 40.0);
+        self.loading.center = self.view.center
+        self.loading.hidesWhenStopped = true
+        self.loading.color = .gray
+        self.loading.hidesWhenStopped = true
+        
+        if #available(iOS 13.0, *) {
+            self.loading.style = .large
+        } else {
+            self.loading.style = .gray
+        }
+        
+        self.view.addSubview(self.loading)
+        
+        self.loading.startAnimating()
+        self.loading.isHidden = false
+    }
+    
+    private func stopLoading() {
+        self.loading.stopAnimating()
+        self.loading.isHidden = true
+    }
 
+}
+
+extension LightBoxViewController: LightboxProtocol {
+    
+    func showLightbox(data: Data?, url: URL?) {
+        
+        DispatchQueue.main.async {
+            if let data = data, let url = url {
+                self.mainWebView?.load(data, mimeType:"text/html", characterEncodingName:"UTF-8", baseURL: url)
+            }
+            
+            self.stopLoading()
+        }
     }
 }
--- Sources/TrustlySDK/Services/TrustlyService.swift
+++ Sources/TrustlySDK/Services/TrustlyService.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 
+protocol LightboxProtocol {
+    func showLightbox(data: Data?, url: URL?)
+}
+
 
 class TrustlyService {
     
@@ -15,6 +19,8 @@ class TrustlyService {
     
     private var sessionCid = "ios wrong sessionCid"
     private var cid = "ios wrong cid"
+    
+    public var delegate: LightboxProtocol?
 
     
     init () {
@@ -90,4 +96,48 @@ class TrustlyService {
         return nil
     }
     
+    public func establishWebView(establishData eD: [AnyHashable : Any]) {
+        
+        var establishData = EstablishDataUtils.prepareEstablish(establishData: eD, cid: cid, sessionCid: sessionCid)
+        
+        if establishData.index(forKey: "metadata.integrationContext") == nil {
+            establishData["metadata.integrationContext"] = Constants.inAppIntegrationContext
+        }
+        
+        do {
+            let environment = try buildEnvironment(
+                resourceUrl: .index,
+                environment: (establishData["env"] ?? "") as! String,
+                localUrl: (establishData["envHost"] ?? "") as! String,
+                paymentType: (establishData["paymentType"] ?? "") as! String,
+                build: Constants.buildSDK
+            )
+
+            loadLightbox(establish: establishData, url: environment.url) { (data, response, error) in
+                
+                if error == nil, let response = response {
+                    self.delegate?.showLightbox(data: data, url: response.url)
+                }
+            }
+            
+        } catch NetworkError.invalidUrl {
+            print("Error: Invalid url.")
+            
+        } catch {
+            print("Unexpected error: \(error).")
+        }
+    }
+    
+    public func chooseIntegrationStrategy(establishData: [AnyHashable : Any], completionHandler: @escaping(String) -> Void) {
+        
+        getTrustlySettingsWith(establish: establishData) { trustlySettings in
+
+            if let settings = trustlySettings?.settings {
+                completionHandler(settings.integrationStrategy)
+                
+            } else {
+                completionHandler(Constants.lightboxContentWebview)
+            }
+        }
+    }
 }
