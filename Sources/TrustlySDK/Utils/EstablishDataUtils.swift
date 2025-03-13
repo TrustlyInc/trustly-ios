1570f1f2a76e370d126b60e04f1584751c15612c
Marcos Rivereto <marcos.rivereto@trustly.com>
2025-03-13 10:19:24 -0300

    Open lightbox in WebView

--- Sources/TrustlySDK/Utils/EstablishDataUtils.swift
+++ Sources/TrustlySDK/Utils/EstablishDataUtils.swift
@@ -131,4 +131,32 @@ struct EstablishDataUtils {
         grp = String(format:"%d", grpInt)
         return grp
     }
+    
+    static func prepareEstablish(establishData eD: [AnyHashable : Any], cid:String, sessionCid: String) -> [AnyHashable : Any] {
+        
+        var establishData = eD
+        
+        establishData["sessionCid"] = sessionCid
+        establishData["metadata.cid"] = cid
+
+        let deviceType = "\(establishData["deviceType"] ?? Constants.deviceType):\(Constants.devicePlatform)"
+        establishData["deviceType"] = deviceType
+        
+        if let lang = establishData["metadata.lang"] as? String {
+            establishData["lang"] = lang
+        }
+        
+        establishData["metadata.sdkIOSVersion"] = Constants.buildSDK
+
+        establishData["returnUrl"] = Constants.returnURL
+        establishData["cancelUrl"] = Constants.cancelURL
+        establishData["version"] = Constants.establishVersion
+        establishData["grp"] = self.getGrp()
+
+        if establishData["paymentProviderId"] != nil {
+            establishData["widgetLoaded"] = "true"
+        }
+        
+        return establishData
+    }
 }
