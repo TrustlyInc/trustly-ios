ed4655bd51098bacb390947ee98e6b88c6e2a83f
Marcos Rivereto <marcos.rivereto@trustly.com>
2025-06-17 17:51:29 -0300

    Add functions to get device information

--- Sources/TrustlySDK/Helpers/DeviceHelper.swift
+++ Sources/TrustlySDK/Helpers/DeviceHelper.swift
@@ -9,15 +9,30 @@ import Foundation
 import UIKit
 
 
-func getDeviceUUID () -> String? {
-    
-    if let uuid = UIDevice.current.identifierForVendor?.uuidString {
-        return uuid
+class DeviceHelper {
+ 
+    static func getDeviceUUID () -> String? {
         
-    } else {
-        print("Unable to retrieve device ID.")
-        
+        if let uuid = UIDevice.current.identifierForVendor?.uuidString {
+            return uuid
+            
+        } else {
+            print("Unable to retrieve device ID.")
+            
+        }
+
+        return nil
     }
 
-    return nil
+    static func systemName() -> String {
+        return UIDevice.current.systemName
+    }
+
+    static func systemVersion() -> String {
+        return UIDevice.current.systemVersion
+    }
+    
+    static func model() -> String {
+        return UIDevice.current.model
+    }
 }
